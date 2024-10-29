8d96f6c69b6838f57a6e3730be16873602e51fce
GianoR130 <gianotti.riccardoaldo@itisgrassi.edu.it>
2024-10-29 12:03:58 +0100

    Update script.js

--- Nascondi/script.js
+++ Nascondi/script.js
@@ -1,16 +1,37 @@
-function mostra()
-
-    {
-
-    let card = document.getElementById("cardNascosta");
-    let bottone = document.getElementById("bottone");
+function mostra1(){
+    
+    let card = document.getElementById("cardNascosta1");
+    let bottone = document.getElementById("bottone1");
     if(card.classList.contains("d-none")){
-
         card.classList.remove("d-none");
         bottone.innerHTML = "Nascondi";
-
     }else{
+        card.classList.add("d-none");
+        bottone.innerHTML = "Mostra";
+    }
+}
 
+function mostra2(){
+    
+    let card = document.getElementById("cardNascosta2");
+    let bottone = document.getElementById("bottone2");
+    if(card.classList.contains("d-none")){
+        card.classList.remove("d-none");
+        bottone.innerHTML = "Nascondi";
+    }else{
+        card.classList.add("d-none");
+        bottone.innerHTML = "Mostra";
+    }
+}
+
+function mostra3(){
+    
+    let card = document.getElementById("cardNascosta3");
+    let bottone = document.getElementById("bottone3");
+    if(card.classList.contains("d-none")){
+        card.classList.remove("d-none");
+        bottone.innerHTML = "Nascondi";
+    }else{
         card.classList.add("d-none");
         bottone.innerHTML = "Mostra";
     }
